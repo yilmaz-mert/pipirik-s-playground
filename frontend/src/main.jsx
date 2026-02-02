f52897bca79183c0cbb7dbcbb24df59878950dae
Mert.Y <mertyilmazgroup@gmail.com>
2026-02-02 23:10:34 +0300

    Fix for vercel

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom' 
 import './index.css'
 import App from './App.jsx'
 
-alert("Main.jsx çalışıyor!");
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 )
